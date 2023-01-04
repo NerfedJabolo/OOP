d30822fab7d6bc046e40151692d79f2b0dccb85f
NerfedJabolo <Ezdlprofit1@gmail.com>
2023-01-04 15:46:08 +0200

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -1,11 +1,9 @@
 #create a Student class
 class Student:
-    __id = 0
     #create init function with private name and id
-    def __init__(self, name):
+    def __init__(self, name, id):
         self.__name = name
-        self.__id = Student.__id
-        Student.__id += 1
+        self.__id = id
     #get name
     def get_name(self):
         return self.__name
@@ -15,9 +13,18 @@ class Student:
     #set name
     def set_name(self, name):
         self.__name = name
+    #set status
+    def set_status(self, status):
+        if status == "Active" or status == "Inactive" or status == "Expelled" or status == "Finished":
+            self.__status = status
+    #get status
+    def get_status(self):
+        return self.__status
+
+    
 
 #create student objects
-student1 = Student("John")
-student2 = Student("Mary")
-student3 = Student("Bob")
+student1 = Student("John", 1)
+student2 = Student("Mary", 2)
+student3 = Student("Bob", 3)
 print(student1.get_id(), student2.get_id(), student3.get_id())
